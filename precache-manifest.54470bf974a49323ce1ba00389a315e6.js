325989262ea422cee5a57f170377272ec35a0595
Dan K <KC135Q@users.noreply.github.com>
2019-01-19 10:39:13 -0500

    Updates

---
+++ precache-manifest.54470bf974a49323ce1ba00389a315e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde89890c7f8e2d7352e",
+    "url": "/jean-clicky/static/js/runtime~main.dde89890.js"
+  },
+  {
+    "revision": "b69dae23e5f282160787",
+    "url": "/jean-clicky/static/js/main.b69dae23.chunk.js"
+  },
+  {
+    "revision": "ab826ed5304424716772",
+    "url": "/jean-clicky/static/js/1.ab826ed5.chunk.js"
+  },
+  {
+    "revision": "b69dae23e5f282160787",
+    "url": "/jean-clicky/static/css/main.10bc0f3b.chunk.css"
+  },
+  {
+    "revision": "6bb507c1f1a6821e095133346bd80c5f",
+    "url": "/jean-clicky/index.html"
+  }
+];
